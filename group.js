fd52acc599f42d412502a57b0545421f3aae3b71
evan10s <evan10s@users.noreply.github.com>
2014-08-03 11:16:18 -0400

    Added check to see if data to group is present
    Added a check to see if the grouping data input box is empty; if so, an
    error message will be shown when the Submit button is pressed

--- group.js
+++ group.js
@@ -79,6 +79,7 @@ function makeGroups() {
         i,
         j,
         k;
+    if(dataToProcess !== "") {
     console.log("Rows length: " + rows.length);
     console.log(maxChoices);
     console.log((maxChoices + 2));
@@ -379,8 +380,7 @@ function makeGroups() {
         groups.push(createGroup(id, person1, "None"));
         console.log("Group " + id + " - Person 1: " + person1 + " - Person 2: None");
         people.splice(0,1);*/
-
-    
+    }
     //Output the groups
     $('#groups').empty(); //Get rid of previous results, if any
     if (groups.length === 0) {
